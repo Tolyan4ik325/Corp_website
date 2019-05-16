6b848aa5ce8a8a3870134bd8a6cfe16e153c8331
Anatolyi Ovrutskyi <a.s.ovrutskyi@gmail.com>
2019-05-16 21:10:52 +0300

    create portfolio migration

---
+++ database/migrations/2019_05_16_180819_create_portfolios_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePortfoliosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('portfolios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('text');
+            $table->string('customer', 150);
+            $table->string('alias', 150);
+
+            $table->string('img');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('portfolios');
+    }
+}
