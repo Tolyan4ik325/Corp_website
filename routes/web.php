37ba4ea21c5e01155504a4b7efe959efe90ce85c
Anatolyi Ovrutskyi <a.s.ovrutskyi@gmail.com>
2019-05-18 11:46:08 +0300

    idnex controller

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,16 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('/', 'IndexController', [
+
+										'only'=>['index'],
+										'names' => [
+											'index'=>'home'
+										]
+
+									]);
