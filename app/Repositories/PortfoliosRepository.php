f15e1c4ff460735c17bf0622b173da925c1b2ba6
Tolyan4ik325 <a.s.ovrutskyi@gmail.com>
2019-07-05 15:33:33 +0300

    create repository to portfolio

---
+++ app/Repositories/PortfoliosRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Corp\Repositories;
+
+use Corp\Portfolio;
+
+class PortfoliosRepository extends Repository {
+
+	public function __construct(Portfolio $portfolio) {
+		$this->model = $portfolio;
+	}
+
+}
+
+?>
