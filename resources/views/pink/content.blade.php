80e42858ecd660007e217e117950f16f088fcafc
Tolyan4ik325 <a.s.ovrutskyi@gmail.com>
2019-07-06 00:42:05 +0300

    create template for projects column4

--- resources/views/pink/content.blade.php
+++ resources/views/pink/content.blade.php
@@ -15,13 +15,13 @@
 				                            <a class="thumb"><img src="{{ asset(env('THEME')) }}/images/projects/0081-385x192.jpg" alt="0081" title="0081" /></a>
 				                            <div class="work-overlay">
 				                                <h3><a href="#">{{ $item->title }}</a></h3>
-				                                <p class="work-overlay-categories"><img src="{{ asset(env('THEME')) }}/images/categories.png" alt="Categories" /> in: <a href="category.html">Brand Identity</a>, <a href="category.html">Web Design</a></p>
+				                                <p class="work-overlay-categories"><img src="{{ asset(env('THEME')) }}/images/categories.png" alt="Categories" /> in: <a href="category.html">{{ $item->filter->title }}</a></p>
 				                            </div>
 				                        </div>
 				                        <div class="work-description">
 				                            <h2><a href="project.html">{{ $item->title }}</a></h2>
 				                            <p class="work-categories">in: <a href="#">{{ $item->filter->title }}</a></p>
-				                            {{ $item->text, 200 }}
+				                            {{ str_limit($item->text, 200) }}
 
 
 				                                <a href="project.html" class="read-more">|| Read more</a>
@@ -32,72 +32,27 @@
 				                    	@continue
 										@endif
 
-									@endforeach
 
 
-				                    
-				                    
+				                    @if($k == 1)
 				                    <div class="portfolio-projects">
-				                        
-				                        <div class="related_project">
+				                    @endif    
+				                        <div class="related_project  {{ ($k==4 ) ? ' related_project_last' : ''}}">
 				                            <div class="overlay_a related_img">
 				                                <div class="overlay_wrapper">
 				                                    <img src="{{ asset(env('THEME')) }}/images/projects/0061-175x175.jpg" alt="0061" title="0061" />						
 				                                    <div class="overlay">
 				                                        <a class="overlay_img" href="{{ asset(env('THEME')) }}/images/projects/0061.jpg" rel="lightbox" title=""></a>
 				                                        <a class="overlay_project" href="project.html"></a>
-				                                        <span class="overlay_title">Love</span>
+				                                        <span class="overlay_title">{{ $item->title }}</span>
 				                                    </div>
 				                                </div>
 				                            </div>
-				                            <h4><a href="project.html">Love</a></h4>
-				                            <p>Nullam volutpat, mauris scelerisque iaculis semper, justo odio rutrum urna, [...]
+				                            <h4><a href="#">{{ $item->title }}</a></h4>
+				                            <p>{{ str_limit($item->text, 200) }}</p>
 				                        </div>
-				                        
-				                        <div class="related_project">
-				                            <div class="overlay_a related_img">
-				                                <div class="overlay_wrapper">
-				                                    <img src="{{ asset(env('THEME')) }}/images/projects/0071-175x175.jpg" alt="0071" title="0071" />						
-				                                    <div class="overlay">
-				                                        <a class="overlay_img" href="{{ asset(env('THEME')) }}/images/projects/0071.jpg" rel="lightbox" title=""></a>
-				                                        <a class="overlay_project" href="project.html"></a>
-				                                        <span class="overlay_title">Kineda</span>
-				                                    </div>
-				                                </div>
-				                            </div>
-				                            <h4><a href="project.html">Kineda</a></h4>
-				                            <p>Nullam volutpat, mauris scelerisque iaculis semper, justo odio rutrum urna, [...]
-				                        </div>
-				                        
-				                        <div class="related_project">
-				                            <div class="overlay_a related_img">
-				                                <div class="overlay_wrapper">
-				                                    <img src="{{ asset(env('THEME')) }}/images/projects/009-175x175.jpg" alt="009" title="009" />						
-				                                    <div class="overlay">
-				                                        <a class="overlay_img" href="{{ asset(env('THEME')) }}/images/projects/009.jpg" rel="lightbox" title=""></a>
-				                                        <a class="overlay_project" href="project.html"></a>
-				                                        <span class="overlay_title">Guanacos</span>
-				                                    </div>
-				                                </div>
-				                            </div>
-				                            <h4><a href="project.html">Guanacos</a></h4>
-				                            <p>Nullam volutpat, mauris scelerisque iaculis semper, justo odio rutrum urna, [...]
-				                        </div>
-				                        
-				                        <div class="related_project_last related_project">
-				                            <div class="overlay_a related_img">
-				                                <div class="overlay_wrapper">
-				                                    <img src="{{ asset(env('THEME')) }}/images/projects/0011-175x175.jpg" alt="0011" title="0011" />						
-				                                    <div class="overlay">
-				                                        <a class="overlay_img" href="{{ asset(env('THEME')) }}/images/projects/0011.jpg" rel="lightbox" title=""></a>
-				                                        <a class="overlay_project" href="project.html"></a>
-				                                        <span class="overlay_title">Miller Bob</span>
-				                                    </div>
-				                                </div>
-				                            </div>
-				                            <h4><a href="project.html">Miller Bob</a></h4>
-				                            <p>Nullam volutpat, mauris scelerisque iaculis semper, justo odio rutrum urna, [...]
-				                        </div>
+				                        @endforeach
+				                       
 				                        
 				                    </div>
 				                </div>
