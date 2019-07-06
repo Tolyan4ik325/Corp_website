57af467dcb6c589363c49f09c16f3f1f4a725158
Tolyan4ik325 <a.s.ovrutskyi@gmail.com>
2019-07-06 23:21:42 +0300

    set interconnection between models

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    public function article() {
+    	return $this->belongsTo('Corp\Article');
+    }
+    public function user() {
+    	return $this->belongsTo('Corp\User');
+    }
 }
