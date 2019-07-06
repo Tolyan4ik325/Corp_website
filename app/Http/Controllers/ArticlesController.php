5891137878a90a25de3dae890cdf656886aa85ce
Anatolyi Ovrutskyi <a.s.ovrutskyi@gmail.com>
2019-07-06 20:51:34 +0300

    create Articles controller, articles main blade

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -4,7 +4,17 @@ namespace Corp\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ArticlesController extends Controller
+class ArticlesController extends SiteController
 {
-    //
+    public function __construct(PortfoliosRepository $p_rep, ArticlesRepository $a_rep) {
+        
+        parent::__construct(new MenusRepository(new \Corp\Menu));
+        
+        $this->p_rep = $p_rep;
+        $this->a_rep = $a_rep;
+        $this->bar = 'right';
+        
+        $this->template = env('THEME').'.articles';
+        
+    }
 }
