ef5c28931f41d6c808113bebb43ef73844f359c0
Tolyan4ik325 <a.s.ovrutskyi@gmail.com>
2019-07-08 00:20:58 +0300

    create last comments template

--- resources/lang/ru/ru.php
+++ resources/lang/ru/ru.php
@@ -7,7 +7,8 @@ return [
 	'comments' =>"Комментарий|Комментария|Комментариев",
 	'read_more' => 'Читать далее',
 	'articles_no' => 'Записей нет',
-	'latest_projects' => 'Последние работы'
+	'latest_projects' => 'Последние работы',
+	'latest_comments' => 'Последние комментарии'
 ]
 
 ?>
--- resources/views/pink/articlesBar.blade.php
+++ resources/views/pink/articlesBar.blade.php
@@ -17,6 +17,27 @@
 				                    	@endif     
 				                </div>
 				            </div>
+
+				            @if(!$comments->isEmpty())
+								<div class="widget-last widget recent-comments">
+				                	<h3>{{ Lang::get('ru.latest_comments')}}</h3>
+				                	<div class="recent-post recent-comments group">
+				                		@foreach($comments as $comment)
+				                			 <div class="the-post group">
+				                        		<div class="avatar">
+				                        			@set($hash, ($comment->email) ? md5($comment->email) : $comment->user->email)
+				                        	    	<img alt="" src="https://www.gravatar.com/avatar/{{ $hash}}?d=mm&s=55" class="avatar" />   
+				                        		</div>
+				                        		<span class="author"><strong><a href="#">{{ isset($comment->user) ? $comment->user->name : $comment->name }}</a></strong> in</span> 
+				                        		<a class="title" href="{{ route('articles.show', ['alias'=> $comment->article->alias])}}"{{ $comment->article->title }}</a>
+				                        		<p class="comment">
+                                        	    {{ $comment->text }} <a class="goto" href="{{ route('articles.show', ['alias'=> $comment->article->alias])}}">&#187;</a>
+                                        		</p>
+				                    		</div>
+				                    	@endforeach
+				                	</div>
+				                </div>
+				            @endif
 				            <div class="widget-last widget recent-comments">
 				                <h3>Recent Comments</h3>
 				                <div class="recent-post recent-comments group">
@@ -54,5 +75,5 @@
 				                        </p>
 				                    </div>
 				                </div>
-				            </div>
+				            
 				            
