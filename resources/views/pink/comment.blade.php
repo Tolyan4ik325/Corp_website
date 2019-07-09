67f6aa8bcd2a676e9883ab0895578bdc2f8c15ed
Tolyan4ik325 <a.s.ovrutskyi@gmail.com>
2019-07-09 14:55:27 +0300

    get id to div comment blade

--- resources/views/pink/comment.blade.php
+++ resources/views/pink/comment.blade.php
@@ -1,6 +1,6 @@
 @foreach($items as $item)
 <li id="li-comment-{{$item->id}}" class="comment even {{ ($item->user_id == $article->user_id) ? 'bypostauthor odd' :: ''}}">
-	<div class="comment-container">
+	<div id="comment-{{$item->id}}" class="comment-container">
 	    <div class="comment-author vcard">
 	        <img alt="" src="images/avatar/unknow.png" class="avatar" height="75" width="75" />
 	        <cite class="fn">Anonymous</cite>                 
