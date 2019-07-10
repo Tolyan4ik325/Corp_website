729a926de18bdadd90e0f92cfab6652d758a2f9e
Tolyan4ik325 <a.s.ovrutskyi@gmail.com>
2019-07-10 13:31:00 +0300

    get setting to hide fields

--- resources/views/pink/article_content.blade.php
+++ resources/views/pink/article_content.blade.php
@@ -42,10 +42,12 @@
 				                <h3 id="comments-title">
 				                    <span>{{ count($article->comments)}}</span> {{ Lang::choice('ru.comments', count($article->comments))}}   
 				                </h3>
+								
+								@if(count($article->comments) > 0)
 
 								@set($com, $article->comments->groupBy('parent_id'))
 
-				                <ol class="commentlist group">
+							 	<ol class="commentlist group">
 
 				                @foreach($com as $k => $comments)
 
@@ -58,6 +60,8 @@
 				                @endforeach
 				                  
 				                </ol>
+
+				                @endif
                                 
 				                <!-- START TRACKBACK & PINGBACK -->
 				                <h2 id="trackbacks">Trackbacks and pingbacks</h2>
@@ -68,12 +72,19 @@
 				                <div id="respond">
 				                    <h3 id="reply-title">Leave a <span>Reply</span> <small><a rel="nofollow" id="cancel-comment-reply-link" href="#respond" style="display:none;">Cancel reply</a></small></h3>
 				                    <form action="sendmail.PHP" method="post" id="commentform">
+				                    	@if(!Auth::check())
 				                        <p class="comment-form-author"><label for="author">Name</label> <input id="author" name="author" type="text" value="" size="30" aria-required="true" /></p>
 				                        <p class="comment-form-email"><label for="email">Email</label> <input id="email" name="email" type="text" value="" size="30" aria-required="true" /></p>
 				                        <p class="comment-form-url"><label for="url">Website</label><input id="url" name="url" type="text" value="" size="30" /></p>
+
+										@endif
+
 				                        <p class="comment-form-comment"><label for="comment">Your comment</label><textarea id="comment" name="comment" cols="45" rows="8"></textarea></p>
 				                        <div class="clear"></div>
 				                        <p class="form-submit">
+											
+											<input id="comment_post_ID" type="hidden" name="comment_post_ID" value="{{$article->id}}" />
+											<input id="comment_parent" type="hidden" name="comment_parent" value="" />
 				                            <input name="submit" type="submit" id="submit" value="Post Comment" />
 				                        </p>
 				                    </form>
