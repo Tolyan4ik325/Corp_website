2ebee3533557960de2ecc8e3b9783c48dcecd08a
Tolyan4ik325 <a.s.ovrutskyi@gmail.com>
2019-07-13 01:54:53 +0300

    add portfolio content

---
+++ resources/views/pink/portfolio_content.blade.php
@@ -0,0 +1,80 @@
+<div id="content-page" class="content group">
+				            <div class="clear"></div>
+				            <div class="posts">
+				                <div class="group portfolio-post internal-post">
+				                    <div id="portfolio" class="portfolio-full-description">
+				                        
+				                        <div class="fulldescription_title gallery-filters">
+				                            <h1>Steep This!</h1>
+				                        </div>
+				                        
+				                        <div class="portfolios hentry work group">
+				                            <div class="work-thumbnail">
+				                                <a class="thumb"><img src="images/projects/0081-700x345.jpg" alt="0081" title="0081" /></a>
+				                            </div>
+				                            <div class="work-description">
+				                                <p>Nullam volutpat, mauris scelerisque iaculis semper, justo odio rutrum urna, at cursus urna nisl et ipsum. Donec dapibus lacus nec sapien faucibus eget suscipit lorem mattis.</p>
+				                                <p>Donec non mauris ac nulla consectetur pretium sit amet rhoncus neque. Maecenas aliquet, diam sed rhoncus vestibulum,<strong> sem lacus ultrice</strong>s est, eu hendrerit tortor nulla in dui. Suspendisse enim purus, euismod interdum viverra eget, ultricies eu est. Maecenas dignissim mauris id est semper suscipit. Suspendisse venenatis vestibulum quam, quis porttitor arcu vestibulum et.</p>
+				                                <p>Sed <a href="http://yourinspirationweb.com/demo/sheeva/work/xmas-icons/#">porttitor eros </a>ut purus elementum a consectetur purus vulputate</p>
+				                                <div class="clear"></div>
+				                                <div class="work-skillsdate">
+				                                    <p class="skills"><span class="label">Skills:</span> Illustrator</p>
+				                                    <p class="workdate"><span class="label">Customer:</span> Steep This!</p>
+				                                    <p class="workdate"><span class="label">Year:</span> 2012</p>
+				                                </div>
+				                            </div>
+				                            <div class="clear"></div>
+				                        </div>
+				                        
+				                        <div class="clear"></div>
+				                        
+				                        <h3>Other Projects</h3>
+				                        
+				                        <div class="portfolio-full-description-related-projects">
+				                            
+				                            <div class="related_project">
+				                                <a class="related_proj related_img" href="#" title="Love"><img src="images/projects/0061-175x175.jpg" alt="0061" title="0061" /></a>
+				                                <h4><a href="#">Love</a></h4>
+				                            </div>
+				                            
+				                            <div class="related_project">
+				                                <a class="related_proj related_img" href="#" title="Kineda"><img src="images/projects/0071-175x175.jpg" alt="0071" title="0071" /></a>
+				                                <h4><a href="#">Kineda</a></h4>
+				                            </div>
+				                            
+				                            <div class="related_project">
+				                                <a class="related_proj related_img" href="#" title="Guanacos"><img src="images/projects/009-175x175.jpg" alt="009" title="009" /></a>
+				                                <h4><a href="#">Guanacos</a></h4>
+				                            </div>
+				                            
+				                            <div class="related_project">
+				                                <a class="related_proj related_img" href="#" title="Miller Bob"><img src="images/projects/0011-175x175.jpg" alt="0011" title="0011" /></a>
+				                                <h4><a href="#">Miller Bob</a></h4>
+				                            </div>
+				                            
+				                            <div class="related_project">
+				                                <a class="related_proj related_img" href="#" title="VItale Premium"><img src="images/projects/0027-175x175.jpg" alt="0027" title="0027" /></a>
+				                                <h4><a href="#">VItale Premium</a></h4>
+				                            </div>
+				                            
+				                            <div class="related_project_last related_project">
+				                                <a class="related_proj related_img" href="#" title="Nili Studios"><img src="images/projects/0034-175x175.jpg" alt="0034" title="0034" /></a>
+				                                <h4><a href="#">Nili Studios</a></h4>
+				                            </div>
+				                            
+				                            <div class="related_project">
+				                                <a class="related_proj related_img" href="#" title="Digitpool Medien"><img src="images/projects/0043-175x175.jpg" alt="0043" title="0043" /></a>
+				                                <h4><a href="#">Digitpool Medien</a></h4>
+				                            </div>
+				                            
+				                            <div class="related_project">
+				                                <a class="related_proj related_img" href="#" title="Octopus"><img src="images/projects/0052-175x175.jpg" alt="0052" title="0052" /></a>
+				                                <h4><a href="#">Octopus</a></h4>
+				                            </div>
+				                            
+				                        </div>
+				                    </div>
+				                    <div class="clear"></div>
+				                </div>
+				            </div>
+				        </div>
