2fd01b90bd3e96a4c33565e5f72da9cc56026f0e
Tolyan4ik325 <a.s.ovrutskyi@gmail.com>
2019-07-13 01:01:13 +0300

    get items from portfolios database

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -4,7 +4,48 @@ namespace Corp\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PortfolioController extends Controller
+use Corp\Repositories\MenusRepository;
+
+use Corp\Repositories\PortfoliosRepository;
+
+class PortfolioController extends SiteController
 {
-    //
+    public function __construct(PortfoliosRepository $p_rep) {
+        
+        parent::__construct(new MenusRepository(new \Corp\Menu));
+        
+        $this->p_rep = $p_rep;
+        $this->template = env('THEME').'.portfolios';
+        
+    }
+
+     public function index()
+    {
+        //
+        
+
+    	$this->title = 'Портфолио';
+    	$this->keywords = 'Портфолио';
+    	$this->meta_desc = 'Портфолио';
+
+    	$portfolios = $this->getPortfolios();
+
+    	dd($portfolios);
+
+        
+         $content = view(env('THEME').'.portfolios_content')->with('portfolios', $portfolios)->render();
+        $this->vars = array_add($this->vars, 'content', $content);
+
+        return $this->renderOutput();
+   
+    }
+
+    public function getPortfolios() {
+    	$portfolios = $this->p_rep->get('*', FALSE, TRUE);
+    	if($portfolios) {
+    		$portfolios->load('filter');
+    	}
+
+    	return $portfolios;
+    }
 }
