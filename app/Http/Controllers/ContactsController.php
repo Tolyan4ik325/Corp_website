4f06cb1a25c4b1c2dd59abf93853712b4930e00c
Tolyan4ik325 <a.s.ovrutskyi@gmail.com>
2019-07-13 14:54:42 +0300

    create contactcontroller

---
+++ app/Http/Controllers/ContactsController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Corp\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactsController extends SiteController
+{
+    //
+     public function __construct() {
+        
+        parent::__construct(new MenusRepository(new \Corp\Menu));
+        
+        $this->bar = 'left';
+
+        $this->template = env('THEME').'.contacts';
+        
+    }
+
+    public function index(Request $request) {
+
+    	$this->title = 'Контакты';
+
+    	$content = view(env('THEME').'.contact_content')->render();
+    	$this->vars = array_add($this->vars, 'content', $content);
+
+    	$this->contentLeftBar = view(env('THEME').'.contact_bar')->render();
+    	$this->vars = array_add($this->vars, 'content', $content);
+
+        return $this->renderOutput();
+    }
+}
