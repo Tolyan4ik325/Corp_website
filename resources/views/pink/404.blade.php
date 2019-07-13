32600a6bac24a327f3400871a34ef90a515bcf04
Tolyan4ik325 <a.s.ovrutskyi@gmail.com>
2019-07-14 02:27:46 +0300

    hendler

--- resources/views/pink/404.blade.php
+++ resources/views/pink/404.blade.php
@@ -2,11 +2,5 @@
 				            <img class="error-404-image group" src="images/features/404.png" title="Error 404" alt="404" />
 				            <div class="error-404-text group">
 				                <p>We are sorry but the page you are looking for does not exist.<br />You could <a href="index.html">return to the home page</a> or search using the search box below.</p>
-				                <form method="get" id="searchform" action="#">
-				                    <div><label class="screen-reader-text" for="s">Search for:</label>
-				                        <input type="text" value="" name="s" id="s" />
-				                        <input type="submit" id="searchsubmit" value="Search" />
-				                    </div>
-				                </form>
 				            </div>
 				        </div>
