723ac17d3f76ceec5d8effbc4adf72f24dab635b
Tolyan4ik325 <a.s.ovrutskyi@gmail.com>
2019-07-14 03:10:19 +0300

    create 404 blade

--- resources/views/pink/404.blade.php
+++ resources/views/pink/404.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 
 <div id="content-index" class="content group">
-				            <img class="error-404-image group" src="images/features/404.png" title="Error 404" alt="404" />
+				            <img class="error-404-image group" src="{{asset(env('THEME'))}}/images/features/404.png" title="Error 404" alt="404" />
 				            <div class="error-404-text group">
 				                <p>We are sorry but the page you are looking for does not exist.<br />You could <a href="index.html">return to the home page</a> or search using the search box below.</p> 
 				            </div>
