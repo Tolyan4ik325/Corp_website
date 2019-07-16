fa77e7fa5eadc05c3e4d5d393b3fc5e1e3db8472
Tolyan4ik325 <a.s.ovrutskyi@gmail.com>
2019-07-17 02:27:31 +0300

    add login to controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -27,6 +27,13 @@ class LoginController extends Controller
      */
 
 
+    
+
+     protected $loginView;
+     
+     protected $username = 'login';
+     
+     
     protected $redirectTo = '/home';
 
     /**
@@ -37,11 +44,20 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+
+        $this->loginView = env('THEME').'.login';
     }
 
     public function showLoginForm()
     {
-        return view(env('THEME').'.login');
+        $view = property_exists($this, 'loginView')
+                    ? $this->loginView : '';
 
+        if (view()->exists($view)) {
+            return view($view)->with('title', 'Вход на сайт');
+        }
+
+        abort(404);
     }
+
 }
