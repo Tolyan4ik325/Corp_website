fde0364e1d6f2c5a0abd65f62f774126f199719a
Anatolyi Ovrutskyi <a.s.ovrutskyi@gmail.com>
2019-07-17 19:18:10 +0300

    add routes for admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -57,6 +57,23 @@ Route::post('login', 'Auth\LoginController@login');
 
 Route::get('logout', 'Auth\LoginController@logout');
 
+
+// Admin
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+
+	// admin
+
+	Route::get('/', ['uses' => 'Admin\IndexController@index', 'as' => 'adminIndex']);
+
+	Route::resource('/articles', 'Admin\ArticlesController');
+
+
+});
+
+
+
+
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
