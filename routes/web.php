2fa37602da39c6d702bb596fa40e80872e906ce4
Tolyan4ik325 <a.s.ovrutskyi@gmail.com>
2019-07-21 16:32:16 +0300

    add login blade and routes

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('/', 'IndexController', [
 
@@ -50,3 +50,7 @@ Route::get('articles/cat/{cat_alias?}',['uses'=>'ArticlesController@index','as'=
 Route::resource('comment', 'CommentController', ['only'=>['store']]);
 
 Route::match(['get', 'post'], '/contacts', ['uses'=>'ContactsController@index', 'as'=>'contacts']);
+
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
