a6b32f21babf0ce2b8985fc259af28adef54cf03
Tolyan4ik325 <a.s.ovrutskyi@gmail.com>
2019-07-23 03:03:35 +0300

    add new models Role and Permission

--- database/migrations/2019_07_22_232606_change_role_user_table.php
+++ database/migrations/2019_07_22_232606_change_role_user_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
@@ -15,11 +14,14 @@ class ChangeRoleUserTable extends Migration
     {
         Schema::table('role_user', function (Blueprint $table) {
             //
-            $table->integer('user_id')->unsigned()->default(1);
-            $table->foreign('user_id')->references('id')->on('users');
-
-            $table->integer('role_id')->default(1);
+            
+            $table->integer('role_id')->unsigned()->default(1);
             $table->foreign('role_id')->references('id')->on('roles');
+            
+            
+            $table->integer('role_id')->unsigned()->default(1);
+            $table->foreign('role_id')->references('id')->on('roles');
+            
         });
     }
 
