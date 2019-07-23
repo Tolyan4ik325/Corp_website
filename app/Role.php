e0b25def9a1beeb2e7324923c04f06d22b82414a
Tolyan4ik325 <a.s.ovrutskyi@gmail.com>
2019-07-23 03:06:46 +0300

    connect roles and users models

--- app/Role.php
+++ app/Role.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     //
+
+    public function users() {
+    	return $this->belongsToMany('Corp\User', 'role_user');
+    }
 }
--- app/User.php
+++ app/User.php
@@ -30,4 +30,8 @@ class User extends Authenticatable
     public function articles()  {
         return $this->hasMany('Corp\Article');
     }
+
+    public function roles()  {
+        return $this->belongsToMany('Corp\Role', 'role_user');
+    }
 }
