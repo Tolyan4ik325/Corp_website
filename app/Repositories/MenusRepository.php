168ee90bc50947eb77ea6921907544aaca559175
Tolyan4ik325 <a.s.ovrutskyi@gmail.com>
2019-07-28 00:56:00 +0300

    select link for portfolio

--- app/Repositories/MenusRepository.php
+++ app/Repositories/MenusRepository.php
@@ -30,6 +30,23 @@ class MenusRepository extends Repository{
 					if($request->input('category_alias') == 'parent') {
 						$data['path'] = route('articles.index');
 					}
+					else {
+						$data['path'] = route('articlesCat', ['cat_alias'=>$request->input('category_alias')]);
+					}
+				}
+				else if($request->input('article_alias')) {
+					$data['path'] = route('articles.show',['alias' => $request->input('article_alias')]);
+				}
+			break;
+			case 'portfolioLink' : 
+				if($request->input('filter_alias')) {
+					if($request->input('filter_alias') == 'parent') {
+						$data['path'] = route('portfolios.index');
+					}
+				}
+
+				else if($request->input('portfolio_alias')) {
+					$data['path'] = route('portfolios.show',['alias' => $request->input('portfolio_alias')]);
 				}
 			break;
 
