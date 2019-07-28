779538f175429eda00ed301c9b4ef937c6abced1
Tolyan4ik325 <a.s.ovrutskyi@gmail.com>
2019-07-28 15:37:28 +0300

    update delete method

--- app/Menu.php
+++ app/Menu.php
@@ -11,4 +11,13 @@ class Menu extends Model
     protected $fillable = [
     	'title','path', 'parent'
     ];
+
+
+    public function delete(array $options = []) {
+
+    	self::where('parent', $this->id)->delete();
+
+
+    	return parent::delete($options);
+    }
 }
